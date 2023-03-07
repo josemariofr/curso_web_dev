138d183324f326a7f51f6b17bb5aaab362c80dd3
Zé Mário <59663635+josemariofr@users.noreply.github.com>
2023-03-07 16:24:12 -0300

    Capitulo 2 Ex_03

---
+++ alunos/jose_mario/capitulo_02/aula_11/ex_03.js
@@ -0,0 +1,12 @@
+
+        var filme = {
+            título: "Anjos e Demônios",
+            diretor: "Ron Howard",
+            elenco: "Tom Hanks, Ewan McGregor, Stellan Skarsgård",
+            anoLancamento: 2009
+
+        };
+        console.log("Título: " + filme.titulo);
+        console.log("Diretor: " + filme.diretor);
+        console.log("Elenco: " + filme.elenco);
+        console.log("Ano de Lançamento: " + filme.anoLancamento);
