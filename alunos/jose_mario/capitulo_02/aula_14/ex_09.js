285dd9b2a991c5d9bc292a3cec4a84cb067f4ada
Zé Mário <59663635+josemariofr@users.noreply.github.com>
2023-03-13 17:48:49 -0300

    Aula 14 ex 06 a 10

---
+++ alunos/jose_mario/capitulo_02/aula_14/ex_09.js
@@ -0,0 +1,5 @@
+// 4 - Crie um algoritmo que receba um número e imprima a tabuada dele de 1 a 10.
+var numero = 7;
+for (var i = 1; i <= 10; i++) {
+    console.log(numero + " x " + i + " = " + numero * i);
+}
---
+++ alunos/jose_mario/capitulo_02/aula_14/ex_10.js
@@ -0,0 +1,5 @@
+// 5 - Crie um algoritmo que receba um número e imprima a tabuada dele de 10 a 1.
+var numero = 20;
+for (var i = 10; i >= 1; i--) {
+    console.log(numero + " x " + i + " = " + numero * i);
+}
