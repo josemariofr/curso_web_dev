e4d8106a2a577701409dad9a4c74761056b223a0
Zé Mário <59663635+josemariofr@users.noreply.github.com>
2023-03-16 16:25:35 -0300

    Exercícios Aula 16

---
+++ alunos/jose_mario/capitulo_02/aula_16/ex_01.js
@@ -0,0 +1,7 @@
+// 1. Crie uma função em JavaScript chamada "saudacao" que recebe um parâmetro "nome" 
+// e retorna a saudação "Olá, nome!".
+
+function saudacao(nome) {
+    return 'Ola ' + nome;
+}
+console.log(saudacao ('Zé Mário!'))
---
+++ alunos/jose_mario/capitulo_02/aula_16/ex_10.js
@@ -0,0 +1,13 @@
+// 10. Crie uma função em JavaScript chamada "verificarPalindromo" que recebe um parâmetro string, "palavra", 
+// e verifica se a palavra é um palíndromo (ou seja, se ela pode ser lida da mesma forma de trás para frente). 
+// Retorne true se for um palíndromo e false caso contrário.
+
+function verificarPalindromo(palavra) {
+    var resultado = palavra.split("").reverse().join("")
+    if(resultado == palavra) {
+        return true
+    }
+    return false
+}
+
+console.log(verificarPalindromo('ama'))
