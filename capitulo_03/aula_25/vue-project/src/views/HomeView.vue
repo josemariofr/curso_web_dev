8d960b4f8f56ab6bbb290ae81b437e5a9befbec5
Diogo Oliveira <diogo.oliveira@groupsoftware.com.br>
2023-04-03 20:49:21 -0300

    aula 25 parte 1

---
+++ capitulo_03/aula_25/vue-project/src/views/HomeView.vue
@@ -0,0 +1,21 @@
+<template>
+  <div class="home">
+    <!-- <img alt="Vue logo" src="../assets/logo.png"> -->
+    <HelloWorld msg="Contador"/>
+    <NovoComponent/>
+  </div>
+</template>
+
+<script lang="ts">
+import { defineComponent } from 'vue';
+import HelloWorld from '@/components/HelloWorld.vue'; // @ is an alias to /src
+import NovoComponent from '@/components/NovoComponent.vue';
+
+export default defineComponent({
+  name: 'HomeView',
+  components: {
+    HelloWorld, 
+    NovoComponent
+  },
+});
+</script>
