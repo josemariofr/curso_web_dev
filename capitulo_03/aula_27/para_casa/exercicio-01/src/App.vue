3ba7e849b08d7b109e759a053cccd75cf4574c71
Diogo Oliveira <diogo.o@icloud.com>
2023-04-11 21:07:18 -0300

    aula_27_para_casa_exercícios_1_e_2

---
+++ capitulo_03/aula_27/para_casa/exercicio-01/src/App.vue
@@ -0,0 +1,29 @@
+<template>
+  <img alt="Vue logo" src="./assets/logo.png">
+  <HelloWorld nome="Diogo Oliveira"/>
+  <CounterComponent/>
+</template>
+
+<script lang="ts">
+import { defineComponent } from 'vue';
+import HelloWorld from './components/HelloWorld.vue';
+import CounterComponent from './components/CounterComponent.vue';
+
+export default defineComponent({
+  name: 'App',
+  components: {
+    HelloWorld, CounterComponent
+  }
+});
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
---
+++ capitulo_03/aula_27/para_casa/exercicio-01/src/components/CounterFilho.vue
@@ -0,0 +1,20 @@
+<template>
+    <div>
+        <h2>Contador: {{ counterfilho }}</h2>
+    </div>
+</template>
+
+<script lang="ts">
+import { defineComponent } from 'vue'
+
+export default defineComponent({
+    name: 'CounterFilho',
+    props: {
+        counterfilho: Number
+    }
+})
+</script>
+
+<style scoped>
+
+</style>
