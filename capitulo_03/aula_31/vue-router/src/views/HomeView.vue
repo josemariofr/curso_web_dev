4856000e06c722c4a37c696be4de1c2874c95cd6
Diogo Oliveira <diogo.o@icloud.com>
2023-04-18 21:34:56 -0300

    aula_31_parte_2

--- capitulo_03/aula_31/vue-router/src/views/HomeView.vue
+++ capitulo_03/aula_31/vue-router/src/views/HomeView.vue
@@ -2,6 +2,11 @@
   <div class="home">
     <!-- <h1 style="color: red">{{msg}}</h1> -->
     <h1>Home</h1>
+
+    <button @click="mostrarTexto = !mostrarTexto">Mostrar Texto</button>
+    <Transition name="my-transition" mode="out-in">
+      <p v-if="mostrarTexto">Este é o texto</p>
+    </Transition>
   </div>
 </template>
 
@@ -10,6 +15,11 @@ import { defineComponent } from "vue";
 
 export default defineComponent({
   name: "HomeView",
+  data() {
+    return {
+      mostrarTexto: true
+    }
+  }
   // props: {
   //   msg: {
   //     type: String,
@@ -18,3 +28,17 @@ export default defineComponent({
   // }
 });
 </script>
+
+<style>
+
+.my-transition-enter-active,
+.my-transition-leave-active {
+  transition: opacity 0.5s ease;
+}
+
+.my-transition-enter-from,
+.my-transition-leave-to {
+  opacity: 0;
+}
+
+</style>
